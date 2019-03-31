e37f05c2915df8a53d41ae632a3efb5da7cbc0af
TheVigor <igor.proskuryakov14@gmail.com>
2019-03-31 20:52:25 +0300

    rename main activity to home activity

--- app/src/main/java/com/noble/activity/dmsiscoming/HomeActivity.kt
+++ app/src/main/java/com/noble/activity/dmsiscoming/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.noble.activity.dmsiscoming
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
